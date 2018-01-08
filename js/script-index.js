4e0e6e3d00681dd97811e2a58a437ae277c83d14
berenisser <berenisser@gmail.com>
2018-01-08 19:24:01 -0300

    etapa 4

--- js/script-index.js
+++ js/script-index.js
@@ -1,22 +1,25 @@
+/*parte 1*/
 $(document).ready( function(){
 	$('#back').remove(); //con .remove eliminamos el icono flecha.
-
-});
-
-	$('#recetas').html(function printNews(){
-		$('#recetas').text('NUEVAS RECETAS')
-
-});
-
 
 //La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 renderHighlightedRecipes(recipesArray);
 
+});
+
+/*parte 2*/
+	//agregando texto nuevas recetas
+	$('#recetas').html(function printNews(){
+		$('#recetas').text('NUEVAS RECETAS')
+});
+
+/*parte 3*/
 /*
 * Función que se encarga de pintar TODAS las recetas que tengan 
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
+	console.log('Recipes: ', recipesArray);
 	for (var i = 0; i < recipesArray.length; i++) {
 		if(recipesArray[i].highlighted == true){
 			renderRecipe(recipesArray[i]);
@@ -24,15 +27,32 @@ function renderHighlightedRecipes(recipesArray) {
 	};
 };
 
-
+/*parte 4*/
 /*
 * Función que se encarga de pintar UNA recetas que tenga 
 * marcado el atributo "highlighted" como TRUE
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-recipe.html"
 */
+
+/*En esta funcion agregamos la lista de imágenes al 
+*div list-recipes de nuestro html, desde jquery
+*/
 function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
+  	$('.list-recipes').append('<a class="item-recipe" href="#">' +
+						'<span class="attribution">' +
+    				'<span class="title-recipe">' + recipe.title + '</span>' +
+    				'<span class="metadata-recipe">' +
+      			'<span class="author-recipe">' + recipe.source.name + '</span>'+
+      			'<span class="bookmarks-recipe">' +
+        		'<span class="icon-bookmark">' +
+        		'</span>' +
+      			'</span>' +
+    				'</span>' +
+  					'</span>' +
+						'<img src="img/recipes/640x480/' + recipe.name + '.jpg" />' + 
+						'</a>');
+  		console.log('Voy a pintar la receta: ', recipe);
 }
 
 
